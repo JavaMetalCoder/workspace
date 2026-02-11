afbfdc289d8b367baadd171fbc5fb15d5c0b584f
JavaMetalCoder <MetalCoder.dev@proton.me>
2026-02-11 21:03:03 +0100

    add

--- labforweb/JavaScript/SecondaLezione/js/script.js
+++ labforweb/JavaScript/SecondaLezione/js/script.js
@@ -8,17 +8,26 @@ const input = document.getElementById("input");
 
 const btn = document.getElementById("inviaBtn");
 
+const err = document.getElementById("errore");
+
 /*ESECUZIONE*/ 
 
 inviaBtn.onclick = function() {
   console.log("Cliccato");
   
   let valoreInput = input.value;
-  if(valoreInput == "") {
-    window.alert("Inserisci un valore ");
-  } else {
+  if(valoreInput.trim().length == 0) {
+    //window.alert("Inserisci un valore ");
+    //err.textContent = "Inserisci contenuto valido! Riprova!"
+    input.classList.add("erroreInput");
 
+    
+
+  } else {
+    //err.innerText = "";
+    input.classList.remove("erroreInput");
     titolo.innerText = valoreInput;
+    
 
   }
 
