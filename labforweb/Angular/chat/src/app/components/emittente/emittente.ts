1ab4e62ec24299bdeb5e9335e73de431026ae956
JavaMetalCoder <MetalCoder.dev@proton.me>
2026-04-23 18:18:21 +0200

    ok

--- labforweb/Angular/chat/src/app/components/emittente/emittente.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-emittente',
-  imports: [],
-  templateUrl: './emittente.html',
-  styleUrl: './emittente.css',
-})
-export class Emittente {}
--- labforweb/Angular/chat/src/app/components/list-message/list-message.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-list-message',
-  imports: [],
-  templateUrl: './list-message.html',
-  styleUrl: './list-message.css',
-})
-export class ListMessage {}
