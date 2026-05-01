3f8c9d38b872268e8d4f293f1a9f345b28215ead
JavaMetalCoder <MetalCoder.dev@proton.me>
2026-05-01 20:10:20 +0200

    feat: enhance add task form with validation and success message display

--- labforweb/Angular/STOPCODE/gestionale-task/src/app/pages/add-page/add-page.ts
+++ labforweb/Angular/STOPCODE/gestionale-task/src/app/pages/add-page/add-page.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { TaskService } from '../../service/task-service';
+import { ITask } from '../../type/task';
+import { RouterLink } from "@angular/router";
 
 
 @Component({
   selector: 'app-add-page',
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   templateUrl: './add-page.html',
   styleUrl: './add-page.css',
 })
-export class AddPage {}
+export class AddPage {
+  private taskService = inject(TaskService);
+
+  successMessage = signal<string | null>(null);
+  
+  onSubmit(form: NgForm): void {
+    if(form.invalid) {
+      return;
+    }
+    const valueTask: ITask = form.value;
+    this.taskService.addTasks(valueTask);
+    
+    this.showSuccessMessage('Task aggiunto correttamente :)');
+    
+    
+    form.resetForm();
+
+    
+  }
+
+  private showSuccessMessage(message: string): void {
+  this.successMessage.set(message);
+
+  setTimeout(() => {
+    this.successMessage.set(null);
+  }, 2500);
+}
+}
+
